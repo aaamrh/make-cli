961fad0c4ccdb11198eea73b31ca31a5458770bb
aaamrh <aaamrh@163.com>
2023-07-19 15:54:56 +0800

    feat: hello world

--- cli3/bin/index.js
+++ cli3/bin/index.js
@@ -1,1 +1,8 @@
 #!/usr/bin/env node
+
+const commander = require("commander");
+const pkg = require("../package.json");
+
+const { program } = commander; // 单例
+
+program.version(pkg.version).parse(process.argv);
