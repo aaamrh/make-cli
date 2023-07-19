ac6010b3c4c20a997161d51ff3bcf4fad52b3749
aaamrh <aaamrh@163.com>
2023-07-19 14:00:08 +0800

    feat: 脚手架启蒙版本

--- cli1/bin/index.js
+++ cli1/bin/index.js
@@ -1,12 +1,26 @@
 #!/usr/bin/env node
 
 const commands = {
-  init() {
-    console.log("init");
+  init(args) {
+    console.log(
+      `command: init, option: ${args.option || "无"}, options 参数: ${
+        args.params || "无"
+      }`
+    );
   },
 };
 
-console.log("ma-cli1: 这是脚手架最基础版本");
+console.log("Welcome ! ma-cli1 是学习脚手架的启蒙版本");
 const argv = require("process").argv;
 
-console.log(require("process").argv);
+const command = argv[2];
+const options = argv.slice(3);
+let [option, params] = options;
+
+option = option?.replace("--", "");
+
+if (commands[command]) {
+  commands[command]({ option, params });
+} else {
+  console.log("请输入有效的命令 暂时只支持 init");
+}
